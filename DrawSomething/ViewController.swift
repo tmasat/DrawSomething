247ac2c55be725498880c7672c15d9b770e242ed
tugberkmasat <tugberkmasat@yahoo.com>
2021-01-10 22:20:20 +0300

    Draw function and X|Y -> 0 / 100 strokePath added.

--- DrawSomething/ViewController.swift
+++ DrawSomething/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
+class Canvas: UIView {
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+
+        let startPoint = CGPoint(x: 0, y: 0)
+        let endPoint = CGPoint(x: 100, y: 100)
+        context.move(to: startPoint)
+        context.addLine(to: endPoint)
+        context.strokePath()
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let canvas = Canvas()
+
+        view.addSubview(canvas)
+        canvas.backgroundColor = .white
+        canvas.frame = view.frame
     }
-
-
 }
 

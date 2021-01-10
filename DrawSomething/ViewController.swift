6c59f789048431476b6eabdb84e887bdef249d3d
tugberkmasat <tugberkmasat@yahoo.com>
2021-01-10 22:03:12 +0300

    Init project

---
+++ DrawSomething/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawSomething
+//
+//  Created by Cagri Tugberk MASAT on 10.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
